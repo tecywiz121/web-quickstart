bf52fa7b5cf409d900e856c3794dadd5ea2ee134
Sam Wilson <tecywiz121@hotmail.com>
2018-10-29 12:19:48 -0400

    Hook up database and rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod db;
+
+use rocket;
+use rocket::fairing::AdHoc;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(AdHoc::on_attach(|rocket| {
+            let db_url = rocket
+                .config()
+                .get_str("database_url")
+                .unwrap_or(concat!(env!("CARGO_MANIFEST_DIR"), "/db.sqlite"));
+
+            let pool = match db::create_pool(db_url) {
+                Ok(x) => x,
+                Err(_) => return Err(rocket),
+            };
+
+            Ok(rocket.manage(pool))
+        }))
+        .launch();
 }
